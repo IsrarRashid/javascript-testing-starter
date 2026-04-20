7b6f40ab626187a1c4d863953e4b25f32097d3e7
IsrarRashid <israrrashid3@gmail.com>
2026-04-21 01:51:16 +0500

    TDD Example

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -28,5 +28,23 @@ describe("fizzBuzz", () => {
 
   it("should return arg as string if it is not divisible by 3 or 5", () => {
     expect(fizzBuzz(19)).toBe("19");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("should return NaN if given empty array", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it("should calculate the average of an array with a single element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  it("should calculate the average of an array with a two elements", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  it("should calculate the average of an array with a three elements", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
   });
 });
